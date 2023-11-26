2c107c98f01ac90ee995105062945af584c52c1d
MrPotatsus <MrPotatsus@yandex.ru>
2023-11-26 16:49:45 +0300

    task2.py is added

---
+++ task2.py
@@ -0,0 +1,18 @@
+def multiply_until_zero():
+    """
+    Считывает числа с клавиатуры и перемножает их до ввода 0.
+
+    Возвращает произведение введенных чисел.
+    """
+    result = 1
+    while True:
+        num = int(input('Введите число (для остановки введите 0): '))
+        if num == 0:
+            break
+        result *= num
+    return result
+
+
+if __name__ == '__main__':
+    result = multiply_until_zero()
+    print(f'Произведение введенных чисел: {result}')
